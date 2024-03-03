e68d04ac3b5a3d0520c068db841ca5523721be22
aleixnieto <aleixnieto@gmail.com>
2024-03-03 18:13:30 +0100

    Preconditioned GMRES working! However it's not optimized at all, we calculate all the arnoldi vectors at each gmres iteration. This needs to be updated.

--- functions.py
+++ functions.py
@@ -1,40 +1,10 @@
 import numpy as np
 from time import time
 
-# def arnoldi(A, b, x0, m, tol=1e-12):
-#     """
-#     This function computes an orthonormal basis V_m = {v_1,...,v_{m+1}} of 
-#     K_{m+1}(A, r^{(0)}) = span{r^{(0)}, Ar^{(0)}, ..., A^{m}r^{(0)}}.
-
-#     Input parameters:
-#     -----------------
-#       A: array_like
-#           An (n x n) array.
-      
-#       b: array_like
-#           Initial vector of length n
-      
-#       m: int
-#           One less than the dimension of the Krylov subspace. Must be > 0.
-      
-#       x0: array_like 
-#           Initial approximation of the solution (length n)
-      
-#       tol: 
-#           Tolerance for convergence
-
-#     Output:
-#     -------
-#       Q: numpy.array 
-#           n x m array, the columns are an orthonormal basis of the Krylov subspace.
-      
-#       H: numpy.array
-#           An (m + 1) x m array. It is the matrix A on basis Q. It is upper Hessenberg.
-#     """
-
+# from preconditioner import iter_precondition
 from preconditioner import iter_precondition
 
-def arnoldi(A, b, x0, m, precondition = False, M = None, tol = 1e-12):
+def arnoldi_og(A, b, x0, m, tol=1e-12):
     """
     This function computes an orthonormal basis V_m = {v_1,...,v_{m+1}} of 
     K_{m+1}(A, r^{(0)}) = span{r^{(0)}, Ar^{(0)}, ..., A^{m}r^{(0)}}.
@@ -64,48 +34,6 @@ def arnoldi(A, b, x0, m, precondition = False, M = None, tol = 1e-12):
       H: numpy.array
           An (m + 1) x m array. It is the matrix A on basis Q. It is upper Hessenberg.
     """
-    
-    # TODO: Now we calculate precondition time at each iteration. The idea is that arnoldi algorithm only provides one 
-    # - iteration at a time so we don't build every time the whole basis from scratch in the GMRES algorithm
-    # - torch?
-    # - instead of A, callable function that calculates Ax for any input vector x?
-    # Check inputs
-    n = A.shape[0]
-    assert A.shape == (n, n) and b.shape == (n,) and x0.shape == (n,), "Matrix and vector dimensions don not match"
-    assert isinstance(m, int) and m > 0, "m must be a positive integer"
-    
-    m = min(m, n)
-    
-    # Initialize matrices
-    V = np.zeros((n, m + 1))
-    H = np.zeros((m + 1, m))
-    
-    # Normalize input vector and use for Krylov vector
-    r0 = b - A @ x0
-    beta = np.linalg.norm(r0)
-    V[:, 0] = r0 / beta
-
-    for k in range(1, m + 1):
-        # Generate a new candidate vector
-        w, _= iter_precondition(A, M, V, k, precondition)
-        
-        # Orthogonalization
-        for j in range(k):
-            H[j, k - 1] = V[:, j] @ w
-            w -= H[j, k - 1] * V[:, j]
-        
-        H[k, k - 1] = np.linalg.norm(w)
-
-        # Check convergence
-        if H[k, k - 1] <= tol:
-            print(f"Converged in {k} iterations.")
-            return V, H
-        
-        # Normalize and store the new basis vector
-        V[:, k] = w / H[k, k - 1]
-    
-    return V, H
-    
     # Check inputs
     n = A.shape[0]
     assert A.shape == (n, n) and b.shape == (n,) and x0.shape == (n,), "Matrix and vector dimensions don not match"
@@ -143,6 +71,151 @@ def arnoldi(A, b, x0, m, precondition = False, M = None, tol = 1e-12):
     
     return V, H
 
+
+def arnoldi(A, b, r0, m, precondition = None, M = None, tol = 1e-12):
+    """
+    This function computes an orthonormal basis V_m = {v_1,...,v_{m+1}} of 
+    K_{m+1}(A, r^{(0)}) = span{r^{(0)}, Ar^{(0)}, ..., A^{m}r^{(0)}}.
+
+    Input parameters:
+    -----------------
+      A: array_like
+          An (n x n) array.
+      
+      b: array_like
+          Initial vector of length n
+      
+      m: int
+          One less than the dimension of the Krylov subspace. Must be > 0.
+      
+      x0: array_like 
+          Initial approximation of the solution (length n)
+      
+      tol: 
+          Tolerance for convergence
+
+    Output:
+    -------
+      Q: numpy.array 
+          n x m array, the columns are an orthonormal basis of the Krylov subspace.
+      
+      H: numpy.array
+          An (m + 1) x m array. It is the matrix A on basis Q. It is upper Hessenberg.
+    """
+    
+    # TODO: Now we calculate precondition time at each iteration. The idea is that arnoldi algorithm only provides one 
+    # - iteration at a time so we don't build every time the whole basis from scratch in the GMRES algorithm
+    # - torch?
+    # - instead of A, callable function that calculates Ax for any input vector x?
+    # Check inputs
+    n = A.shape[0]
+    assert A.shape == (n, n) and b.shape == (n,) and r0.shape == (n,), "Matrix and vector dimensions don not match"
+    assert isinstance(m, int) and m > 0, "m must be a positive integer"
+    
+    m = min(m, n)
+    
+    # Initialize matrices
+    V = np.zeros((n, m + 1))
+    H = np.zeros((m + 1, m))
+    
+    # Normalize input vector and use for Krylov vector
+    
+    beta = np.linalg.norm(r0)
+    V[:, 0] = r0 / beta
+
+    for k in range(1, m + 1):
+        # Generate a new candidate vector
+        w, _= iter_precondition(A, M, V, k, precondition)
+        
+        # Orthogonalization
+        for j in range(k):
+            H[j, k - 1] = V[:, j] @ w
+            w -= H[j, k - 1] * V[:, j]
+        
+        H[k, k - 1] = np.linalg.norm(w)
+
+        # Check convergence
+        if H[k, k - 1] <= tol:
+            print(f"Converged in {k} iterations.")
+            return V, H
+        
+        # Normalize and store the new basis vector
+        V[:, k] = w / H[k, k - 1]
+    
+    return V, H
+    
+def arnoldi_one_iter(A, b, r0, m, precondition = False, M = None, tol = 1e-12):
+    """
+    This function computes an orthonormal basis V_m = {v_1,...,v_{m+1}} of 
+    K_{m+1}(A, r^{(0)}) = span{r^{(0)}, Ar^{(0)}, ..., A^{m}r^{(0)}}.
+
+    Input parameters:
+    -----------------
+      A: array_like
+          An (n x n) array.
+      
+      b: array_like
+          Initial vector of length n
+      
+      m: int
+          One less than the dimension of the Krylov subspace. Must be > 0.
+      
+      x0: array_like 
+          Initial approximation of the solution (length n)
+      
+      tol: 
+          Tolerance for convergence
+
+    Output:
+    -------
+      Q: numpy.array 
+          n x m array, the columns are an orthonormal basis of the Krylov subspace.
+      
+      H: numpy.array
+          An (m + 1) x m array. It is the matrix A on basis Q. It is upper Hessenberg.
+    """
+    
+    # TODO: Now we calculate precondition time at each iteration. The idea is that arnoldi algorithm only provides one 
+    # - iteration at a time so we don't build every time the whole basis from scratch in the GMRES algorithm
+    # - torch?
+    # - instead of A, callable function that calculates Ax for any input vector x?
+    # Check inputs
+    n = A.shape[0]
+    assert A.shape == (n, n) and b.shape == (n,) and r0.shape == (n,), "Matrix and vector dimensions don not match"
+    assert isinstance(m, int) and m > 0, "m must be a positive integer"
+    
+    m = min(m, n)
+    
+    # Initialize matrices
+    V = np.zeros((n, m + 1))
+    H = np.zeros((m + 1, m))
+    
+    # Normalize input vector and use for Krylov vector
+    
+    beta = np.linalg.norm(r0)
+    V[:, 0] = r0 / beta
+
+    for k in range(1, m + 1):
+        # Generate a new candidate vector
+        w, _= iter_precondition(A, M, V, k, precondition)
+        
+        # Orthogonalization
+        for j in range(k):
+            H[j, k - 1] = V[:, j] @ w
+            w -= H[j, k - 1] * V[:, j]
+        
+        H[k, k - 1] = np.linalg.norm(w)
+
+        # Check convergence
+        if H[k, k - 1] <= tol:
+            print(f"Converged in {k} iterations.")
+            return V, H
+        
+        # Normalize and store the new basis vector
+        V[:, k] = w / H[k, k - 1]
+    
+    return V, H
+    
 def back_substitution(A, b):
     """
     Solve a linear system using back substitution.
--- preconditioner.py
+++ preconditioner.py
@@ -2,43 +2,47 @@ from enum import Enum, auto
 
 from time import time
 
-from scipy.sparse import diags as sp_diags
+from scipy.sparse import diags as sp_diags, tril as sp_tril, eye as sp_eye, triu as sp_triu
 from scipy.sparse.linalg import spsolve
 
 class PreconditionEnum(Enum):
-    JACOBI = auto()
-    SYMMETRIC_GAUSS_SEIDEL = auto()
-
+    JACOBI = auto(),
+    GAUSS_SEIDEL = auto(),
+    SYMMETRIC_GAUSS_SEIDEL = auto(),
+    
 def initial_precondition(A, precondition, r0):
+    
+    n = A.shape[0]
+    
     # total precondition time
     total_precondition_time = 0
     precondition_start_time = time()
     match precondition:
         case PreconditionEnum.JACOBI:
-            M = sp_diags(A.diagonal(), offsets=0, format="csr")
-            r = spsolve(M, r0)
+            M = sp_diags(A.diagonal(), offsets=0, format = "csr")
+            r0 = spsolve(M, r0)
           
         case PreconditionEnum.SYMMETRIC_GAUSS_SEIDEL:
             D_vector = A.diagonal()
             D_inv_vector = 1 / D_vector
             D_inv = sp_diags(D_inv_vector, offsets=0, format="csr")
 
-            L = sp_eye(m) + sp_tril(A, k=-1, format="csr") @ D_inv
+            L = sp_eye(n) + sp_tril(A, k=-1, format = "csr") @ D_inv
             U = sp_triu(A, format="csr")
             M = [L, U]
 
-            z = spsolve(L, r_original)
-            r = spsolve(U, z)
+            z = spsolve(L, r0)
+            r0 = spsolve(U, z)
             
         # default case (None)
         case _:
             M = None
-            r = r0
+            r0 = r0
 
     precondition_end_time = time()
     total_precondition_time += (precondition_end_time - precondition_start_time)
 
-    return M, r, total_precondition_time
+    return M, r0, total_precondition_time
 
 def iter_precondition(A, M, V, k, precondition):
     
@@ -54,7 +58,7 @@ def iter_precondition(A, M, V, k, precondition):
             
         case PreconditionEnum.SYMMETRIC_GAUSS_SEIDEL:
             L, U = M
-            z = spsolve(L, A @ V[:, k])
+            z = spsolve(L, A @ V[:, k - 1])
             w = spsolve(U, z)
 
         case _:
